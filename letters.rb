87c070f596b4ea00b4045a300c8a0b88d82a1495
Taj Calles <tajcalles@gmail.com>
2018-01-05 12:11:59 -0700

    new solution

--- letters.rb
+++ letters.rb
@@ -1,29 +1,6 @@
-require 'pry'
+@final_arr = []
+puts "\nInput characters seperated by spaces"
 
-def unicode_convert(string)
-  newsymbols = []
-  symbols = (string).to_sym 
-  symbols.each do |char|
-      char = symbols.sort_by{|key, value| value}.to_a
-  end
-end 
+gets.strip.split(" ").each { |n| n.each_byte { |c| @final_arr.push(Hash[n, c]) } }
 
-def run
-  puts "***LETTER DECODER***"
-  puts '1) Your Input'
-  puts '2) Exit'
-  case gets.to_i
-    when 1
-      puts "What's Your Input?"
-      unicode_convert(gets.strip)
-    when 2
-      puts "Goodbye!"
-      exit
-    else
-      puts 'Bad Input. Try Again.'
-      run
-  end
-  run
-end
-
-run
+puts "\nConverted letters: #{@final_arr}\n "
